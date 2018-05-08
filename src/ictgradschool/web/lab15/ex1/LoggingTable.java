115ec583bad29792fe8b29cc6e2153d0f167179a
Cameron Grout <cameron.grout@waikato.ac.nz>
2018-05-09 01:52:57 +1200

    Initial Commit

---
+++ src/ictgradschool/web/lab15/ex1/LoggingTable.java
@@ -0,0 +1,22 @@
+package ictgradschool.web.lab15.ex1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoggingTable extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+
+        pw.print("<p>Display your table and form here</p>");
+
+        pw.close();
+    }
+}
---
+++ src/ictgradschool/web/lab15/ex1/LoggingTableNewEntry.java
@@ -0,0 +1,23 @@
+package ictgradschool.web.lab15.ex1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoggingTableNewEntry extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+        PrintWriter pw = response.getWriter();
+
+        pw.print("<h1>405 - GET method not supported</h1>");
+
+        pw.close();
+    }
+}
